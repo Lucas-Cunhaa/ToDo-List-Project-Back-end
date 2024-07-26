3035a034da537f4f79cdbd4cde1a3f77bec9981a
lucas cunha <lucasgalvao260@gmail.com>
2024-07-25 21:33:18 -0300

    Adding user class

--- src/api/models/User.ts
+++ src/api/models/User.ts
@@ -1,3 +1,15 @@
 class User {
-    
+    private readonly Id : number;
+    private readonly Name : string; 
+    private readonly Email : string; 
+    private readonly Password : string;
+    Foregin_Task : string;  
+
+    constructor(id : number, name : string, email : string, password : string, foregin_Task : string) {
+        this.Id = id 
+        this.Name = name 
+        this.Email = email
+        this.Password = password
+        this.Foregin_Task = foregin_Task
+    }
 }
