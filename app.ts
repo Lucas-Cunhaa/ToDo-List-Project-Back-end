00b761bf71ecca28a19ec00ee0c7d353413eb763
lucas cunha <lucasgalvao260@gmail.com>
2024-08-07 19:12:00 -0300

    fix: fixing the packed.json file, now the nodemo is working

--- app.ts
+++ app.ts
@@ -3,11 +3,11 @@ import cors from 'cors'
 import routes from './routes'
 const app = Express()
 
-app.use(cors)
+app.use(cors())
 app.use(Express.json())
 app.use(routes)
 
-app.listenerCount("3011" , () => {
+app.listen("3011" , () => {
     console.log("Executed on 3011 dor")
 })
 
