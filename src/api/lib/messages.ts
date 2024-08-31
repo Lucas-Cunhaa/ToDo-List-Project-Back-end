40382043a7e7775a79cfa434439b83bb0fde6ae5
lucas cunha <lucasgalvao260@gmail.com>
2024-08-31 10:52:00 -0300

    feat: editing the well done login message, adding the redirecting message

--- src/api/lib/messages.ts
+++ src/api/lib/messages.ts
@@ -2,7 +2,7 @@ export const MESSAGES = {
     USER_EXISTS: 'User already exists',
     REGISTER_SUCCESS: 'Register successfully',
     REGISTER_FAIL: 'Failed to register user',
-    LOGIN_SUCCESS: 'Login successfully',
+    LOGIN_SUCCESS: 'Login successfully redirecting...',
     INVALID_CREDENTIALS: 'Invalid credentials',
     DELETE_SUCCESS: 'User has been deleted',
     DELETE_FAIL: 'Error on delete User',
